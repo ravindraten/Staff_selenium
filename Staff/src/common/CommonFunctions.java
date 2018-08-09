de28f56e4b865fde540eb762f53637e586f6f6f2
ravindra <ravindra.nayak@coosto.com>
2018-08-09 12:20:43 +0200

    updated selenium options

--- Staff/src/common/CommonFunctions.java
+++ Staff/src/common/CommonFunctions.java
@@ -14,8 +14,10 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -24,60 +26,63 @@ import org.testng.Assert;
 
 public class CommonFunctions {
 	//public WebDriver driver = new ChromeDriver();
-	 protected DesiredCapabilities capability;
+	 //protected DesiredCapabilities capability;
 	 public RemoteWebDriver driver;
 	
 	 public void openBrowser(String browserName) throws InterruptedException, MalformedURLException{
 		 
 		 try {
 				if (browserName.equalsIgnoreCase("firefox")) {
-					capability = DesiredCapabilities.firefox();
-					driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capability);
+					FirefoxOptions optionsF = new FirefoxOptions();
+					driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), optionsF);
 				    driver.manage().deleteAllCookies();
 				    driver.get("http://127.0.0.1:8080");
 				    driver.manage().window().maximize();
 				    driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
 				} else if (browserName.equalsIgnoreCase("chrome")) {
-					System.setProperty("webdriver.chrome.driver","C:\\Windows\\chromedriver.exe");
+					System.setProperty("webdriver.chrome.driver","\\Users\\ravindra\\Downloads\\chromedriver");
 					//driver = new ChromeDriver();
-					capability = DesiredCapabilities.chrome();
-					driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capability);
+					ChromeOptions options = new ChromeOptions();
+					driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
 				    driver.manage().deleteAllCookies();
 				    driver.get("http://127.0.0.1:8080");
 				    driver.manage().window().maximize();
 				    driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);  
-				}else if (browserName.equalsIgnoreCase("internet_explorer")) {
-					System.setProperty("webdriver.ie.driver","C:\\Windows\\IEDriverServer.exe");
-					//driver = new ChromeDriver();
-					capability = DesiredCapabilities.internetExplorer();
-					driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capability);
-				    driver.manage().deleteAllCookies();
-				    driver.get("http://127.0.0.1:8080");
-				    driver.manage().window().maximize();
-				    driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
-				   
-				}else if (browserName.equalsIgnoreCase("MicrosoftEdge")) {
-					//System.setProperty("webdriver.ie.driver","C:\\Windows\\IEDriverServer.exe");
-					System.setProperty("webdriver.edge.driver","C:\\Program Files (x86)\\Microsoft Web Driver\\MicrosoftWebDriver.exe");
-					//driver = new InternetExplorerDriver();
-					capability = DesiredCapabilities.edge();
-					Map<String, Object> edgeOptions = new HashMap<String, Object>();
-					capability.setCapability(EdgeOptions.CAPABILITY, edgeOptions);
-					driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capability);
-				    driver.manage().deleteAllCookies();
-				    driver.get("http://127.0.0.1:8080");
-				    driver.manage().window().maximize();
-				    driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
 				}
+//				else if (browserName.equalsIgnoreCase("internet_explorer")) {
+//					System.setProperty("webdriver.ie.driver","C:\\Windows\\IEDriverServer.exe");
+//					//driver = new ChromeDriver();
+//					capability = DesiredCapabilities.internetExplorer();
+//					driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capability);
+//				    driver.manage().deleteAllCookies();
+//				    driver.get("http://127.0.0.1:8080");
+//				    driver.manage().window().maximize();
+//				    driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+//				   
+				//}
+//				    else if (browserName.equalsIgnoreCase("MicrosoftEdge")) {
+//					//System.setProperty("webdriver.ie.driver","C:\\Windows\\IEDriverServer.exe");
+//					System.setProperty("webdriver.edge.driver","C:\\Program Files (x86)\\Microsoft Web Driver\\MicrosoftWebDriver.exe");
+//					//driver = new InternetExplorerDriver();
+//					capability = DesiredCapabilities.edge();
+//					Map<String, Object> edgeOptions = new HashMap<String, Object>();
+//					capability.setCapability(EdgeOptions.CAPABILITY, edgeOptions);
+//					driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capability);
+//				    driver.manage().deleteAllCookies();
+//				    driver.get("http://127.0.0.1:8080");
+//				    driver.manage().window().maximize();
+//				    driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+//				}
 			
 			} catch (WebDriverException e) {
 				System.out.println(e.getMessage());
 			}
 		}
 //		 /
-		  
+		 
 
-	
+
+
 	public void login() throws InterruptedException, MalformedURLException{
 		  driver.findElement(By.linkText("login")).click();
 		  driver.findElement(By.cssSelector("#username")).sendKeys("admin");
